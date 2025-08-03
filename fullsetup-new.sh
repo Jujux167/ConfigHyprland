6745939122c864e56f1ea3492c30ff6f0bd848a9
Julien LEVRIER <102378545+Jujux167@users.noreply.github.com>
2025-08-03 23:20:42 +0200

    aaaa

--- fullsetup-new.sh
+++ fullsetup-new.sh
@@ -42,48 +42,69 @@ main() {
     install_base_packages
 
     print_step "3" "Installation d'Hyprland et composants"
-    install_hyprland_packages
+    install_hyprland
 
-    print_step "4" "Installation des thèmes et icônes"
-    install_theme_packages
+    print_step "4" "Installation de Waybar"
+    install_waybar
 
     print_step "5" "Installation des applications"
-    install_applications
+    install_apps
 
-    print_step "6" "Installation des drivers NVIDIA"
-    install_gpu_packages
+    print_step "6" "Installation audio PipeWire"
+    install_audio
 
-    print_step "7" "Installation de l'AUR helper"
+    print_step "7" "Installation réseau & Bluetooth"
+    install_network
+
+    print_step "8" "Installation gestion batterie"
+    install_power
+
+    print_step "9" "Installation thèmes & icônes"
+    install_themes
+
+    print_step "10" "Installation pilotes GPU"
+    install_gpu_drivers
+
+    print_step "11" "Installation Ly (login manager)"
+    install_login_manager
+
+    print_step "12" "Installation Rofi"
+    install_rofi
+
+    print_step "13" "Installation de l'AUR helper (yay)"
     install_yay
 
-    print_step "8" "Installation des paquets AUR"
+    print_step "14" "Installation des polices"
+    install_fonts
+
+    print_step "15" "Installation des paquets AUR"
     install_aur_packages
 
-    print_step "9" "Configuration d'Hyprland"
+    print_step "16" "Configuration d'Hyprland"
     setup_hyprland_config
 
-    print_step "10" "Configuration de Waybar"
+    print_step "17" "Configuration de Waybar"
     setup_waybar_config
 
-    print_step "11" "Configuration de Rofi"
+    print_step "18" "Configuration de Rofi"
     setup_rofi_config
 
-    print_step "12" "Configuration de Kitty"
+    print_step "19" "Configuration de Kitty"
     setup_kitty_config
 
-    print_step "13" "Configuration d'Hyprlock"
+    print_step "20" "Configuration d'Hyprlock"
     setup_hyprlock
 
-    print_step "14" "Configuration des wallpapers"
+    print_step "21" "Configuration des wallpapers"
     setup_wallpaper
 
-    print_step "15" "Activation des services"
+    print_step "22" "Activation des services"
     enable_services
 
-    print_step "16" "Configuration finale"
+    print_step "23" "Configuration finale"
     setup_xinitrc
 
-    print_step "17" "Nettoyage du système"
+    print_step "24" "Nettoyage du système"
     cleanup_system
 
     # Conclusion stylée
